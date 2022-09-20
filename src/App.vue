f11daab7f3094f01a8039a00410b30f344796c4c
heshaojun <keepword_heshaojun@hotmail.com>
2022-09-20 23:04:36 +0800

    提交联动代码

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,95 @@
 <script setup>
-import { onMounted, ref, getCurrentInstance } from "vue";
-const show = ref(false);
-const { proxy } = getCurrentInstance();
-const toPoint = (id) => {
-  show.value = false;
-  window.location.hash = id;
-  document.getElementById(id);
+const scrollMehod1 = (event) => {
+  let limit = event.target.scrollTop + event.target.clientHeight / 2
+  let children = event.target.children
+  for (let i = 0; i < children.length; i++) {
+    let item = children[i]
+    if (limit > item.offsetTop && limit < (item.offsetTop + item.offsetHeight)) {
+      let name = item.getAttribute("name")
+      let targetparent = document.getElementById('scroll2')
+      let targets = targetparent.children
+      for (let j = 0; j < targets.length; j++) {
+        let target = targets[j]
+        if (target.getAttribute('name') === name) {
+          targetparent.scrollTop = target.offsetTop - (targetparent.clientHeight - target.offsetHeight) / 2
+          target.setAttribute("active", "true")
+        } else {
+          target.removeAttribute("active")
+        }
+      }
+    }
+  }
 };
-const scrollMehod = (x) => {
-  let el = document.getElementById("fdasfdsafsa");
-  console.log(el.offsetTop);
+const clickMethod = (event) => {
+  let target = document.getElementById("scroll1")
+  for (let i = 0; i < target.children.length; i++) {
+    let item = target.children[i]
+    if (item.getAttribute('name') === event.target.getAttribute('name')) {
+      target.scrollTop = item.offsetTop
+      break
+    }
+  }
+}
 
-  console.log(x.target);
-};
-onMounted(() => {
-  window.addEventListener("scroll", scrollMehod, true);
-});
 </script>
 
 <template>
-  <div class="pdf-viewer-page" id="fdasfdsafsa">
-    <div class="head-bar" @click="show = true">fdafda</div>
-    <van-popup
-      v-model:show="show"
-      position="left"
-      style="width: 70%; height: 100%"
-    >
-      <div>
-        <div
-          style="
-            width: 100%;
-            background-color: aquamarine;
-            position: sticky;
-            top: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          "
-        >
-          目录
-        </div>
-        <div @click="toPoint('#id1')">fsdafdsa1</div>
-        <div @click="toPoint('#id2')">fsdafdsa2</div>
-        <div @click="toPoint('#id3')">fsdafdsa3</div>
-        <div @click="toPoint('#id4')">fsdafdsa4</div>
+  <div style="width:100%;height:100%;display:flex;overflow: hidden;">
+    <div style="width:50%;overflow: auto;height: 100%;background-color: burlywood;" @scroll="scrollMehod1" id="scroll1"
+      class="scroll1">
+      <div name="blok1"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok1
       </div>
-    </van-popup>
-    <div style="height:100%;overflow:auto;background-color: blue;">
-      f
+      <div name="blok2"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok2
+      </div>
+      <div name="blok3"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok3
+      </div>
+      <div name="blok4"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok4
+      </div>
+      <div name="blok5"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok5
+      </div>
+      <div name="blok6"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;">blok6
+      </div>
+    </div>
+    <div style="flex:1;overflow: auto;height: 100%;background-color: azure;scroll-behavior: smooth;" id="scroll2"
+      class="scroll2">
+      <div name="blok1"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok1
+      </div>
+      <div name="blok2"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok2
+      </div>
+      <div name="blok3"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok3
+      </div>
+      <div name="blok4"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok4
+      </div>
+      <div name="blok5"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok5
+      </div>
+      <div name="blok6"
+        style="height:500px;border: 1px solid red;display: flex; align-items: center;justify-content: center;"
+        @click="clickMethod">blok6
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.pdf-viewer-page {
-  background-color: aqua;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-  
+.scroll2 {
+  div[active='true'] {
+    background-color: red;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Vant from 'vant';
 import router from "@/router";
 import store from "@/store";
+import '@/style/main.scss'
 import 'vant/lib/index.css';
 
 
