69f1c1b5cac357c5c3d21c1133d512e0f2045a1a
heshaojun <keepword_heshaojun@hotmail.com>
2022-09-25 11:03:44 +0800

    添加部分代码

--- src/views/main/home/panels/Carousel.vue
+++ src/views/main/home/panels/Carousel.vue
@@ -1,20 +1,30 @@
 <script setup>
 import { onMounted, getCurrentInstance, reactive, ref } from "vue";
-const { proxy } = getCurrentInstance()
-const componentHeght = ref()
+const { proxy } = getCurrentInstance();
+const componentHeght = ref();
+const swipeList = ref(["1","2","3"])
 onMounted(() => {
-    let currentComponent = proxy.$refs['panel-carousel']
-    componentHeght.value = currentComponent.clientWidth / 2 + "px"
-})
+  let currentComponent = proxy.$refs["panel-carousel"];
+  componentHeght.value = currentComponent.clientWidth / 2 + "px";
+});
 </script>
 <template>
-    <div class="panel-carousel" ref="panel-carousel" :style="{'height':componentHeght}">
-        
-    </div>
+  <div
+    class="panel-carousel"
+    ref="panel-carousel"
+    :style="{ height: componentHeght }"
+  >
+    <van-swipe :autoplay="3000" indicator-color="white">
+        <van-swipe-item v-for="index,item in swipeList" :key="index">
+                <div>{{item}}</div>
+        </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .panel-carousel {
-    background-color: aqua;
+    width: 100%;
+    background-color: white;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base:'/appWeb/',
     plugins: [vue(), legacy({
         targets: ['defaults', 'not IE 11', 'Chrome 63']
     }), createSvgIconsPlugin({
