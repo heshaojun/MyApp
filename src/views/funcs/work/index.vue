37f60f7a123a1b871b69245b277bf017e3677be8
heshaojun <keepword_heshaojun@hotmail.com>
2022-09-29 17:25:43 +0800

    Update index.vue

--- src/views/funcs/work/index.vue
+++ src/views/funcs/work/index.vue
@@ -7,18 +7,30 @@ const router = useRouter();
 const active = ref(false);
 const viewCache = ref([]);
 onMounted(() => {
-    if (route.path === "/work") {
-        router.replace("/work/createByMe")
+    switch (route.path) {
+        case "/work":
+            router.replace("/work/createByMe");
+            break;
+        case "/work/createByMe":
+            active.value = 0;
+            break;
+        case "/work/handleByMe":
+            active.value = 1;
+            break;
+        case "/work/checkByMe":
+            active.value = 2;
+            break;
+
     }
 });
 </script>
 <template>
     <div class="work">
         <van-nav-bar title="工作" left-arrow left-text="返回" :placeholder="true" />
-        <van-tabs :border="true" color="#70B603">
+        <van-tabs :border="true" v-model:active="active" color="#70B603">
             <van-tab title="我创建的" to="/work/createByMe" />
-            <van-tab title="我验收的" to="/work/checkByMe" />
             <van-tab title="我处理的" to="/work/handleByMe" />
+            <van-tab title="我验收的" to="/work/checkByMe" />
         </van-tabs>
         <div style="flex:1;overflow: hidden">
             <div style="height:100%;overflow: auto;">
