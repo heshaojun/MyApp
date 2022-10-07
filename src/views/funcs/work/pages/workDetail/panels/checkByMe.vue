45b9ef22f3f4f1e0b82816db0e8a44e145c6f1a3
heshaojun <keepword_heshaojun@hotmail.com>
2022-10-07 17:04:17 +0800

    Update checkByMe.vue

--- src/views/funcs/work/pages/workDetail/panels/checkByMe.vue
+++ src/views/funcs/work/pages/workDetail/panels/checkByMe.vue
@@ -1,16 +1,19 @@
 <script setup>
 import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 const props = defineProps({
   workId: String,
 });
+const router = useRouter();
 
 onMounted(() => {});
 </script>
 <template>
-  <div class="check-by-me">我验收的工作props{{ props.workId }}</div>
+  <div class="check-by-me"></div>
 </template>
 <style lang="scss" scoped>
 .check-by-me {
   width: 100%;
+  height: 100%;
 }
 </style>
