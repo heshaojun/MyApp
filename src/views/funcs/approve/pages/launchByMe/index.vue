811598fffe415b6adde8d19bc5379cf6c5667ca8
heshaojun <keepword_heshaojun@hotmail.com>
2022-10-14 21:21:54 +0800

    Update index.vue

--- src/views/funcs/approve/pages/launchByMe/index.vue
+++ src/views/funcs/approve/pages/launchByMe/index.vue
@@ -1,9 +1,21 @@
 <script setup name="launchByMe">
+import { ref } from "vue";
+//列表加载状态
+const listLoading = ref(false);
+//列表数据加载完成状态
+const finishedLoad = ref(false);
+//列表数据
+const listData = ref([])
+//加载数据方法
+const loadData_ = () => {
 
+}
 </script>
 <template>
     <div class="launch-by-me">
-        
+        <van-list v-model:loading="listLoading" :finished="finishedLoad" @load="loadData_()">
+
+        </van-list>
     </div>
 </template>
 <style lang="scss" scoped>
